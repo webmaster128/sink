9ca48b6ee3fff120e2ac541bd83f2cf7004b37ec
Simon Warta <simon@warta.it>
2024-09-10 14:45:27 +0200

    Further cleanup

--- devtools/check_workspace.sh
+++ devtools/check_workspace.sh
@@ -3,15 +3,6 @@ set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
 cargo check
-
-for contract in nois-drand nois-payment nois-proxy nois-proxy-governance-owned; do
-  (
-    cd "./contracts/$contract"
-    cargo check --features library
-  )
-done
-
 cargo test
 
 cargo clippy --all-targets
-cargo clippy --all-targets --features governance_owned
