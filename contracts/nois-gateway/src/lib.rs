e48825f4203ecc3afcec09b770bd23809bd62609
Simon Warta <simon@warta.it>
2024-09-10 14:42:18 +0200

    Remove unused contracts and config

--- contracts/nois-gateway/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod attributes;
-mod drand_archive;
-mod job_id;
-mod request_router;
-
-pub mod contract;
-pub mod error;
-pub mod msg;
-pub mod state;
--- packages/misc/src/lib.rs
+++
@@ -1,3 +0,0 @@
-mod mainnet_addresses;
-
-// No exports here. Don't make this crate a dependency of anything.
