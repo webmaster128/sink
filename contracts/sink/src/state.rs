7af8c6580230891abbcc61ec2b38c2fc18a14345
Simon Warta <simon@warta.it>
2024-09-20 23:25:52 +0200

    Test storey migration

--- contracts/sink/src/state.rs
+++ contracts/sink/src/state.rs
@@ -1,12 +1,6 @@
 use cosmwasm_schema::cw_serde;
 use cosmwasm_std::{Addr, Coin, Timestamp};
-use cw_storage_plus::{Item, Map};
-
-/// A history of all burns.
-/// This is a map from primary ID to the value.
-pub const ASHES: Map<u32, Ash> = Map::new("ashes");
-/// The last used primary ID
-pub const ASHES_LAST_ID: Item<u32> = Item::new("ashes_id");
+use cw_storey::containers::Column;
 
 #[cw_serde]
 pub struct Ash {
@@ -15,3 +9,5 @@ pub struct Ash {
     /// Point in time (block time) when the Ash was created
     pub time: Timestamp,
 }
+
+pub const ASHES: Column<Ash> = Column::new(7);
